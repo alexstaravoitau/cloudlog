ae90d9f89f0b2f9c14623b5981770b27912b12c5
Alex Staravoitau <alex.staravoitau@gmail.com>
2016-12-24 20:20:19 +0000

    Update version number.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='cloudlog',
-    version='0.1',
+    version='0.1.1',
     description='A simple logger that duplicates console logs to a file, Dropbox and Telegram.',
     url='http://github.com/navoshta/cloudlog',
     author='Alex Staravoitau',
