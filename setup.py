e3247b751e58045323761dab31def8f1409bb5c2
Alex Staravoitau <alex.staravoitau@gmail.com>
2016-12-24 18:26:49 +0000

    Add requirements and `setup.py`.

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='cloudlog',
+    version='0.1',
+    description='A simple logger that duplicates console logs to a file, Dropbox and Telegram.',
+    url='http://github.com/navoshta/cloudlog',
+    author='Alex Staravoitau',
+    author_email='alex.staravoitau@gmail.com',
+    license='MIT',
+    packages=['cloudlog'],
+    install_requires=[
+        'dropbox',
+        'matplotlib',
+        'twx.botapi'
+    ],
+    zip_safe=False
+)
